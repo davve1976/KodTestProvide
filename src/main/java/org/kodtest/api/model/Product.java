09f51cc20c0077f3b89dfd8d5a347059dbb97a08
davve1976 <davidstenberg@gmail.com>
2024-05-15 00:24:24 +0200

    added product

---
+++ src/main/java/org/kodtest/api/model/Product.java
@@ -0,0 +1,114 @@
+package org.kodtest.api.model;
+
+public class Product {
+
+	private Long id;
+	private String title;
+	private double price;
+	private String description;
+	private String category;
+	private String image;
+	private Rating rating;
+
+	public Product(Long id, String title, double price, String description, String category, String image,
+			Rating rating) {
+		this.id = id;
+		this.title = title;
+		this.price = price;
+		this.description = description;
+		this.category = category;
+		this.image = image;
+		this.rating = rating;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public String getCategory() {
+		return category;
+	}
+
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public String getImage() {
+		return image;
+	}
+
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+
+	public Rating getRating() {
+		return rating;
+	}
+
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
+	}
+
+	private class Rating {
+		double rate;
+		long count;
+		Rating(double rate, long count) {
+			this.rate = rate;
+			this.count = count;
+		}
+		public double getRate() {
+			return rate;
+		}
+		public void setRate(double rate) {
+			this.rate = rate;
+		}
+		public long getCount() {
+			return count;
+		}
+		public void setCount(long count) {
+			this.count = count;
+		}
+		
+	}
+}
